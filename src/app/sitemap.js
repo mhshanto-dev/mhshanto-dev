331ba71912abca331153e1b1a3d5bd55aa281a2e
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-05-08 14:40:32 +0600

    seo: add /resume to sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -19,5 +19,11 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
